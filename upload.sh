3d57bf837bba7730dbc1ad971fad9e9e62115d3f
Gordon Guthrie <gordon@vixo.com>
2012-10-07 21:29:16 +0100

    tweaking auto publishing shell files

---
+++ upload.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/gordon/jekyll-blog/_site
+echo "tarring and zipping html"
+rm blog.tar.gz
+tar -cvf blog.tar *
+gzip blog.tar
+echo "uploading zipped html"
+scp blog.tar.gz gordon@hypernumbers.com:/hn/files-www/vixo/blog
+cd /home/gordon/jekyll-blog
+echo "over and out..."
