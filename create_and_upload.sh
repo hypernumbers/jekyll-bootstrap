cf0340422126b42082a8bbf82cc0e3b24c59633c
Gordon <gordon@hypernumbers.com>
2012-12-10 21:44:39 +0000

    tweaks to Vixo website

--- create_and_upload.sh
+++ create_and_upload.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # make the site with google analytics
-cd /home/gordon/jekyll-blog/
+cd /home/gordon/jekyll-bootstrap/
 jekyll --safe
 # now tar and copy up the site
-cd /home/gordon/jekyll-blog/_site
+cd /home/gordon/jekyll-bootstrap/_site
 echo "tarring and zipping html"
 rm blog.tar.gz
 tar -cvf blog.tar *
 gzip blog.tar
 echo "uploading zipped html"
 scp blog.tar.gz gordon@hypernumbers.com:/hn/files-www/vixo2/
-cd /home/gordon/jekyll-blog
+cd /home/gordon/jekyll-bootstrap
 echo "over and out..."
